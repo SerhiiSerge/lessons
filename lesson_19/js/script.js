9970b9546534ebb632c04f54fed4bbce379545ca
Serhii <41901317+SerhiiSerge@users.noreply.github.com>
2023-04-27 22:32:32 +0300

    Add files via upload

--- lesson_19/js/script.js
+++ lesson_19/js/script.js
@@ -6,10 +6,6 @@ document.addEventListener('click', function (e) {
         document.documentElement.classList.toggle('form-opened');
         e.preventDefault();
     }
-})
-
-document.addEventListener('click', function (e) {
-    const targetElement = e.target;
     if (targetElement.closest('.rating__close')) {
         document.documentElement.classList.remove('form-opened');
         e.preventDefault();
