9d0c0689785bf2f2bf900a1460781ab89d2dab23
Serhii <41901317+SerhiiSerge@users.noreply.github.com>
2023-05-11 23:09:25 +0300

    Add files via upload

--- lesson_21/js/script.js
+++ lesson_21/js/script.js
@@ -1,9 +1,25 @@
 'use strict';
 
-document.addEventListener('click', function (e) {
+document.addEventListener('click', (e) => {
     const targetElement = e.target;
     if (targetElement.closest('.menu-icon')) {
         document.documentElement.classList.toggle('menu-open');
-        e.preventDefault();
+        document.documentElement.classList.remove('search-open');
     }
-})
+    if (targetElement.closest('.search__button')) {
+        const searchOpen = document.documentElement.classList.toggle('search-open');
+        const searchButton = document.querySelector('.search__button');
+        if (searchOpen) {
+            searchButton.type = 'submit';
+        } else {
+            searchButton.type = 'button';
+        }
+    }
+});
+
+const headerSearch = document.querySelector('.header__search');
+headerSearch.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (!headerSearch.value) return;
+    task.value = '';
+});
